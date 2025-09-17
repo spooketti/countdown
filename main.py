823eeaab5b82603ffbbab06af0e523018458993b
Jonathan Liu <jonliu168@gmail.com>
2025-09-16 22:03:06 -0700

    forgot to add the port

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+@app.route("/")
+def hello():
+    return "Hello, World!"
+app.run(host='0.0.0.0', port=8000)
 
 
 import discord
